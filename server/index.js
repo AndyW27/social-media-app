a38bf36fc5ca024ba5d083edeed10931072bb7fe
AndyW827 <andy.wong.km27@gmail.com>
2022-12-15 23:02:14 -0800

    feat: routes & middlewares for register & login, verify token middleware, user model / chore: added images to public/assets

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import authRoutes from './routes/auth.js';
+import { register } from './controllers/auth.js';
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -31,6 +33,12 @@ const storage = multer.diskStorage({
 	},
 });
 const upload = multer({ storage });
+
+// ROUTES WITH FILES
+app.post('/auth/register', upload.single('picture'), register);
+
+// ROUTES
+app.use('/auth', authRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
