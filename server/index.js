d69a5a3ad495586ecabcdcee3f5b37bd7fc4cbac
AndyW827 <andy.wong.km27@gmail.com>
2022-12-21 14:25:54 -0800

    feat: routes & middlewares for posts / chore: added mock data for user & post

--- server/index.js
+++ server/index.js
@@ -9,7 +9,13 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
 import userRoutes from './routes/users.js';
+import postRoutes from './routes/posts.js';
 import { register } from './controllers/auth.js';
+import { createPost } from './controllers/posts.js';
+import { verifyToken } from './middleware/auth.js';
+import User from './models/User.js';
+import Post from './models/Post.js';
+import { users, posts } from './data/index.js';
 
 // CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url);
@@ -20,7 +26,7 @@ app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
-app.use(morgan('common'));
+app.use(morgan('dev'));
 app.use(cors());
 app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
@@ -37,10 +43,12 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post('/auth/register', upload.single('picture'), register);
+app.post('/posts', verifyToken, upload.single('picture'), createPost);
 
 // ROUTES
 app.use('/auth', authRoutes);
-app.use('/user', userRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001;
@@ -51,5 +59,8 @@ mongoose
 	})
 	.then(() => {
 		app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+		// User.insertMany(users);
+		// Post.insertMany(posts);
 	})
 	.catch((error) => console.log(`${error} did not connect`));
